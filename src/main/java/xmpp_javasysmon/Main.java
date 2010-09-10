bcd7a625956270d56eda97209314d0906978f2e6
James Lewis <jlewis@jalewis.thoughtworks.com>
2010-09-10 11:36:44 +0100

    adding build and making user configurable

--- src/main/java/xmpp_javasysmon/Main.java
+++ src/main/java/xmpp_javasysmon/Main.java
@@ -6,14 +6,16 @@ public class Main {
     public static void main(String[] args) {
         new Main().run(
                 System.getProperty("xmppServerAddress", "jalewis.thoughtworks.com"),
-                System.getProperty("chatroomName", "performance@chatrooms.jalewis.thoughtworks.com")
+                System.getProperty("chatroomName", "performance@chatrooms.jalewis.thoughtworks.com"),
+                System.getProperty("jabberUsername", "server"),
+                System.getProperty("jabberPassword", "server")
         );
     }
 
-    public void run(String xmppServerAddress, String chatroomName) {
+    public void run(String xmppServerAddress, String chatroomName, String jabberUsername, String jabberPassword) {
 
         XmppClient xmppClient = new XmppClient();
-        xmppClient.connect(xmppServerAddress, chatroomName, "server", "server");
+        xmppClient.connect(xmppServerAddress, chatroomName, jabberUsername, jabberPassword);
 
         ReportingCpuMonitor monitor = new ReportingCpuMonitor();
         monitor.startTiming(xmppClient);
